2bbe594794c892f7e67284cf73ba6d14353ed9a2
Aheli kar <ahelikar9@gmail.com>
2025-08-25 15:45:47 +0530

    Update hacker_8.sql

--- hacker_8.sql
+++ hacker_8.sql
@@ -1,1 +1,32 @@
 """
+
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+
+      STATION
+Field   Type
+ID      NUMBER
+CITY    VARCHAR2(21)
+STATE   VARCHAR2(2)
+LAT N   NUMBER
+  """
+#The code
+*/SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE '%a'
+  AND CITY NOT LIKE '%e'
+  AND CITY NOT LIKE '%i'
+  AND CITY NOT LIKE '%o'
+  AND CITY NOT LIKE '%u'
+  AND CITY NOT LIKE '%A'
+  AND CITY NOT LIKE '%E'
+  AND CITY NOT LIKE '%I'
+  AND CITY NOT LIKE '%O'
+  AND CITY NOT LIKE '%U'
+
+
+
